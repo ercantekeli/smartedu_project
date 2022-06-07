35b9e56a51cb012f257b3eff098ec2dd9257a9cb
ercantekeli <ercantekeli@outlook.com>
2022-06-07 17:07:05 +0300

    16.lesson finished

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -1,20 +1,44 @@
 from multiprocessing import context
 from unicodedata import category
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Course, Category, Tag
 
-def course_list(request):
-    courses = Course.objects.all().order_by('-date')
-    categories = Category.objects.all()
+# def course_list(request):
+#     courses = Course.objects.all().order_by('-date')
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
+
+#     context = {
+#         'courses': courses,
+#         'categories': categories,
+#         'tags': tags,
+
+
+#     }
+#     return render(request, 'courses.html', context)
+
+def course_list(request, category_slug=None, tag_slug=None):
+    category_page = None
+    tag_page = None
+    categories = categories = Category.objects.all()
     tags = Tag.objects.all()
+
+    if category_slug != None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        courses = Course.objects.filter(available=True, category=category_page)
+    
+    elif tag_slug != None:
+        tag_page = get_object_or_404(Tag, slug=tag_slug)
+        courses = Course.objects.filter(available=True, tags=tag_page)
+
+    else:
+        courses = Course.objects.all().order_by('-date')
 
     context = {
         'courses': courses,
         'categories': categories,
         'tags': tags,
-
-
-    }
+        }
     return render(request, 'courses.html', context)
 
 def course_detail(request, category_slug, course_id):
@@ -26,8 +50,8 @@ def course_detail(request, category_slug, course_id):
     return render(request, 'course.html', context)
 
 
-def category_list(request, category_slug):
-    courses = Course.objects.all().filter(category__slug=category_slug)
+def search(request):
+    courses = Course.objects.filter(name__contains=request.GET['search'])
     categories = Category.objects.all()
     tags = Tag.objects.all()
 
@@ -39,14 +63,31 @@ def category_list(request, category_slug):
     return render(request, 'courses.html', context)
 
 
-def tag_list(request, tag_slug):
-    courses = Course.objects.all().filter(tags__slug=tag_slug)
-    categories = Category.objects.all()
-    tags = Tag.objects.all()
 
-    context={
-        'courses': courses,
-        'categories': categories,
-        'tags': tags,
-    }
-    return render(request, 'courses.html', context)
+# def category_list(request, category_slug):
+#     courses = Course.objects.all().filter(category__slug=category_slug)
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
+
+#     context={
+#         'courses': courses,
+#         'categories': categories,
+#         'tags': tags,
+#     }
+#     return render(request, 'courses.html', context)
+
+# def tag_list(request, tag_slug):
+#     courses = Course.objects.all().filter(tags__slug=tag_slug)
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
+
+#     context={
+#         'courses': courses,
+#         'categories': categories,
+#         'tags': tags,
+#     }
+#     return render(request, 'courses.html', context)
+
+
+
+
